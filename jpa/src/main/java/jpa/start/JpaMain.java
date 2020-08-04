eecdedef4ee4d2e9e0b39d89f78cce3068bfbc1a
dhlee3994 <dhlee3994@gmail.com>
2020-08-04 21:43:55 +0900

    jpa..

---
+++ jpa/src/main/java/jpa/start/JpaMain.java
@@ -0,0 +1,46 @@
+package jpa.start;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            logic(em);
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(EntityManager em) {
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("동현");
+        member.setAge(29);
+
+        em.persist(member);
+        member.setAge(27);
+
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember = " + findMember.getUsername() + ", age = " + findMember.getAge());
+
+        // List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+        List<Member> members = query.getResultList();
+
+        System.out.println("members.size : " + members.size());
+        em.remove(member);
+    }
+}
