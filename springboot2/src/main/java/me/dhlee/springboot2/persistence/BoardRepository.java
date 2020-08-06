d2a81d41ba65da89ae29071ffa3321ebca1f6f0a
dhlee3994 <dhlee3994@gmail.com>
2020-08-06 21:51:09 +0900

    Study...

--- springboot2/src/main/java/me/dhlee/springboot2/persistence/BoardRepository.java
+++ springboot2/src/main/java/me/dhlee/springboot2/persistence/BoardRepository.java
@@ -3,20 +3,24 @@ package me.dhlee.springboot2.persistence;
 import me.dhlee.springboot2.domain.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
 
-public interface BoardRepository extends CrudRepository<Board, Long> {
+public interface BoardRepository extends CrudRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
+    @Query("SELECT b FROM Board b WHERE b.title LIKE %?1% AND b.bno > 0 ORDER BY b.bno DESC")
     public List<Board> findBoardByTitle(String title);
-
+    @Query("SELECT b FROM Board b WHERE b.content LIKE %:content% AND b.bno > 0 ORDER BY b.bno DESC")
+    public List<Board> findbyContent(@Param("content") String content);
     public Collection<Board> findByWriter(String writer);
-
     public Collection<Board> findByWriterContaining(String writer);
-
     public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
-
     public Page<Board> findByBnoGreaterThan(Long bno, Pageable paging);
+
+
 
 }
--- springboot2/src/test/java/me/dhlee/springboot2/persistence/BoardRepositoryTest.java
+++ springboot2/src/test/java/me/dhlee/springboot2/persistence/BoardRepositoryTest.java
@@ -1,6 +1,8 @@
 package me.dhlee.springboot2.persistence;
 
+import com.querydsl.core.BooleanBuilder;
 import me.dhlee.springboot2.domain.Board;
+import me.dhlee.springboot2.domain.QBoard;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -75,6 +77,16 @@ class BoardRepositoryTest {
     }
 
     @Test
+    public void testFindByTitle2() {
+        repository.findBoardByTitle("17").forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void testFindByContent() {
+        repository.findBoardByTitle("17").forEach(board -> System.out.println(board));
+    }
+
+    @Test
     public void testFindByWriter() {
         repository.findByWriter("지은이10").forEach(board -> System.out.println(board));
     }
@@ -122,4 +134,31 @@ class BoardRepositoryTest {
     public void testDelete() {
         repository.deleteById(1L);
     }
+
+    // querydsl
+    @Test
+    public void testPredicate() {
+        String type = "t";
+        String keyword = "17";
+        BooleanBuilder builder = new BooleanBuilder();
+
+        QBoard qBoard = QBoard.board;
+        if (type.equals("t")) {
+            builder.and(qBoard.title.like("%" + keyword + "%"));
+        }
+
+        builder.and(qBoard.bno.gt(0L));
+
+        // Pageable pageable = PageRequest.of(0, 10);
+        Page<Board> result = repository.findAll(builder, new PageRequest(0, 10));
+
+        System.out.println("PAGE SIZE : " + result.getSize());
+        System.out.println("TOTAL PAGES : " + result.getTotalPages());
+        System.out.println("TOTAL COUNT : " + result.getTotalElements());
+        System.out.println("NEXT : " + result.nextPageable());
+
+        List<Board> list = result.getContent();
+        list.forEach(board -> System.out.println(board));
+    }
+
 }
