510308fc64e11d6e9af16d4055e82c8420c554e9
dhlee3994 <dhlee3994@gmail.com>
2020-08-17 18:16:56 +0900

    study...

---
+++ toby/src/toby/user/dao/DaoFactory.java
@@ -0,0 +1,30 @@
+package toby.user.dao;
+
+public class DaoFactory {
+    public UserDao userDao() {
+        /*
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+        return userDao;
+        */
+        return new UserDao(connectionMaker());
+    }
+
+
+    /*
+    public AccountDao accountDao() {
+        return new AccountDao(new DConnectionMaker());
+    }
+
+    public MessageDao messageDao() {
+        return new MessageDao(new DConnectionMaker());
+    }
+    만약 일렇게 DAO가 점점 더 많아지면 ConnectionMaker 구현 클래스를 선정하고 생성하는 코드의 중복이 일어난다.
+    => 별도의 메소드로 뽑아내자
+    => connectionMaker()메소드로 뽑아냈다
+    */
+
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
+}
--- toby/src/toby/user/dao/UserDao.java
+++ toby/src/toby/user/dao/UserDao.java
@@ -5,39 +5,10 @@ import toby.user.domain.User;
 import java.sql.*;
 
 public class UserDao {
-    /*
-    1. 자유로운 확장이 안됨
-    2. DB 커넥션을 제공하는 클래스가 어떤것인지 UserDao가 구체적으로 알고있어야 함
-    => 인터페이스 도입
-    private SimpleConnectionMaker connectionMaker;
-
-    public UserDao() {
-        connectionMaker = new SimpleConnectionMaker();
-    }
-    */
-
     private ConnectionMaker connectionMaker;
 
-    public UserDao() {
-        connectionMaker = new DConnectionMaker(); // 인터페이스 뿐 아니라 구체적인 클래스까지 알아야 한다는 문제 발생
-    }
-
-    public static void main(String[] args) throws Exception {
-        UserDao dao = new UserDao();
-
-        User user = new User();
-        user.setId("dhlee");
-        user.setName("이동현");
-        user.setPassword("천재");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId() + " 조회 성공");
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
